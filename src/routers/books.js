e47c7ac23eba87e6d0c3c2ba33cad43efa2b3fdf
PointWolfAnj <anojan31@gmail.com>
2022-03-29 10:33:39 +0100

    C

--- src/routers/books.js
+++ src/routers/books.js
@@ -16,6 +16,78 @@ booksRouter.get('/', (req, res) => {
 
   const selectAllBooksQuery = "SELECT * FROM books"
 
+ /*Support for optional query parameters. Depending on the combination
+  of parameters supplied, we want to generate different where clauses:
+   
+    /books                                - SELECT * FROM books
+    /books?type=fiction                   - SELECT * FROM books WHERE type='Fiction'
+    /books?topic=history                  - SELECT * FROM books WHERE topic='history'
+    /books?type=Non-Fiction&topic=history - SELECT * FROM books WHERE topic='history AND type='Non-Fiction' */
+  
+  /*Create an array to store the values we'll provide to the database as 
+    our query place holder params (the values for $1, $2 etc.). These will be
+    different depending on what query string filters the client specifies */
+  const selectValues = []
+
+  /*We'll use this array to keep track of the where clauses we need to add
+  as we check the query parameters. For every WHERE clause we need to add
+  we'll record the column we want to query, and also the value we want to
+  search for on that column */
+  const queries = []
+
+  /*If there is a type query param present (i.e. /books?type=fiction) then
+    add that to our queries array */
+  if(req.query.type) {
+    queries.push({col:'type', val: req.query.type})
+  }
+
+  /*If there is a topic query param present (i.e. /books?topic=horror) then
+    add that to our queries array */
+  if(req.query.topic) {
+    queries.push({col:'topic', val: req.query.topic})
+  }
+  
+  /*If we have any queries specified, then update our SQL statement
+  to add the WHERE clauses we need, and also update the selectValues
+  array to contain the actual values we want to use in place of the 
+  $1, $2 etc. */
+  if(queries.length>0) {
+
+    let whereClauses = []
+    
+    //For each query, add the WHERE clause we'll need in to the whereClauses array
+    //
+    //For example, if queries contains:
+    // [ { col:'type', vale:'Fiction'} ]
+    //we will generate: 
+    // [ 'type = $1' ]
+    //
+    //If queries contains:
+    // [ { col:'type', vale:'Fiction'}, { col:'topic', value:'horror'} ]
+    //we will generate: 
+    // [ 'type = $1', 'topic = $2' ]
+    //
+    queries.forEach( (query, index) => {
+      
+      /* Get index+1, because we want our placeholders ($1, $2) etc. to start at 
+      1, not 0 like the array index */
+      whereClauses.push(`${query.col} = $${index+1}`)
+
+      /*Add the value of the query in to the selectValues array. This make sure
+      that the indexes of the values in the array match up with the place holder
+      values we are generating on the line above. */
+      selectValues.push(query.val) 
+    })
+      
+    /*Update our SQL statement to add on the WHERE clauses
+    we've created in the whereClauses array - we use join so that
+    ' AND ' is used to separate each clause (note the spaces). */
+    selectAllBooksQuery += ' WHERE ' + whereClauses.join(' AND ')
+  }
+  
+  console.log(selectValues)
+  console.log(selectAllBooksQuery)
+
   /*Using the query method to send a SQL query
   to the database. This is asynchronous - so
   we use our "then" callbacks to handle the
@@ -84,6 +156,72 @@ booksRouter.get('/:id', (req, res) => {
       console.log(error)
     })
 })
+
+//DELETE /books/10
+booksRouter.delete("/:bookId", (req, res) => {
+    const deleteBooksQuery = `DELETE FROM books WHERE id = $1 RETURNING *`
+  
+    const deleteValues = [
+      req.params.bookId, //$1 - the book Id
+    ]
+  
+    db.query(deleteBooksQuery, deleteValues)
+      .then((databaseResult) => {
+        if (databaseResult.rowCount === 0) {
+          res.status(404)
+          res.json({ error: "book does not exist" })
+        } else {
+          res.json({ book: databaseResult.rows[0] })
+        }
+      })
+      .catch((error) => {
+        console.log(error)
+        res.status(500)
+        res.json({ error: "unexpected error" })
+      })
+  })
+  
+  booksRouter.put("/:bookId", (req, res) => {
+    
+    const updateBooksQuery = `
+      UPDATE books SET 
+        title = $1,
+        type = $2,
+        author = $3,
+        topic = $4,
+        publicationDate = $5,
+        pages = $6
+      WHERE id = $7
+      RETURNING *`
+  
+    const updateValues = [
+      req.body.title, //$1 = title
+      req.body.type, //$2 = type
+      req.body.author, //$3 = author
+      req.body.topic, //$4 = topic
+      req.body.publicationDate, //$5 = publicationDate
+      req.body.pages, //$6 = pages
+      req.params.bookId, //$7 = bookId
+    ]
+  
+    db.query(updateBooksQuery, updateValues)
+      .then(databaseResult => {
+        console.log(databaseResult)
+        if(databaseResult.rowCount===0) {
+          res.status(404)
+          res.json({error: 'book does not exist'})
+        }
+        else {
+          res.json({book: databaseResult.rows[0]})
+        }
+      })
+      .catch(error => {
+        console.log(error)
+        res.status(500)
+        res.json({error: 'unexpected error'})
+      })
+  
+  })
 
 //POST /books - Adds a new book
 booksRouter.post('/', (req, res) => {
--- src/routers/pets.js
+++ src/routers/pets.js
@@ -21,6 +21,27 @@ petsRouter.get('/', (req, res) => {
     })
 })
 
+petsRouter.get('/breeds', (req, res) => {
+    let getBreedsRequest = `SELECT DISTINCT breed FROM pets`
+    let query = []
+  
+    if(req.query.type) {
+      getBreedsRequest += ` WHERE type = $1`
+      query.push(req.query.type)
+    }
+  
+    db
+    .query(getBreedsRequest, query)
+    .then(result => {
+        res.json({breeds: result.rows})
+    })
+    .catch((error) => {
+        res.status(500)
+        res.json({error: "Unexpected Error"})
+    })
+    })
+
+
 petsRouter.get('/:id', (req, res) => {
   const dbData = 'SELECT * FROM pets WHERE id = $1'
   const queryValues = [req.params.id]
@@ -56,5 +77,67 @@ RETURNING *`
       res.json({ error: 'unexpected error' })
     })
 })
+
+petsRouter.put("/:id", (req, res) => {
+    const putSQL = 'UPDATE pets SET name = $1,  age = $2,  type = $3,  breed = $4,  microchip = $5 WHERE id = $6 RETURNING *'
+
+    const petValues = [
+      req.body.name,
+      req.body.age,
+      req.body.type,
+      req.body.breed,
+      req.body.microchip,
+      req.params.id
+  ]
+
+    db.query(putSQL, petValues)
+    .then(result => {
+    res.json({pets: result.rows[0]})
+   })
+    .catch((error) => {
+    res.status(500)
+    res.json({error: "Unexpected Error"})
+})
+})
+
+petsRouter.patch("/:id", (req, res) => {
+  let patchSQL = 'UPDATE pets SET '
+  const petFields = ["name", "age", "type", "breed", "microchip"]
+
+  let petValues = []
+  let counter = 1
+
+  for (const field of petFields) {
+    if(req.body[field]){
+      patchSQL += `${field} = $${counter}, `
+      counter++
+      petValues.push(req.body[field])
+  }  
+  }
+
+  patchSQL = patchSQL.substring(0, (patchSQL.length - 2))
+  
+  petValues.push(req.params.id)
+  patchSQL += ` WHERE id = $${counter} RETURNING *`
+
+  db.query(patchSQL, petValues)
+  .then(results => res.json({pets: results.rows[0]}))
+  .catch((error) => {
+    res.status(500)
+    res.json({error: "Unexpected Error"})
+  })
+ });
+
+petsRouter.delete("/:id", (req, res) => {
+ const deleteSQL = "DELETE FROM pets WHERE id = $1 RETURNING *"
+
+ db.query(deleteSQL, [req.params.id])
+ .then(results => res.json({pets: results.rows[0]}))
+ .catch((error) => {
+   res.status(500)
+   res.json({error: "Unexpected Error"})
+ })
+});
+
 
 module.exports = petsRouter
